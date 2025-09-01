97754d415ad80835eeb3b5e6bed8f6a774905d63
kseniyamanuilava <kseniya1manuilava@gmail.com>
2025-09-01 15:54:27 +0300

    fix for program that displays last name, first name and profession

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
-      String firstName = "Kseniya" , lastName = "Manuilava", jobTitle = "QA engeneer";
+      String firstName = "Kseniya";
+      String lastName = "Manuilava";
+      String jobTitle = "QA engeneer";
 
-      System.out.println(firstName +" " + lastName + " works as a " + jobTitle);
+      System.out.println(firstName +" " + lastName + " works as a " + jobTitle + ".");
+
+
         }
     }
