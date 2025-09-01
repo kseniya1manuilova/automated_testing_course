c440554923a75191be26a7cd7312e52332ddb579
kseniyamanuilava <kseniya1manuilava@gmail.com>
2025-09-01 16:32:16 +0300

    added and initialized variables of different types

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,27 @@ public class Main {
 
       System.out.println(firstName +" " + lastName + " works as a " + jobTitle + ".");
 
+      byte age = 25;
+      short yearOfBirth = 2000;
+      int phoneNumber = 291116756;
+      double BodyTemperature = 36.6;
 
+      float height;
+      height = 1.7F;
+
+      boolean newMember;
+      newMember = true;
+
+      char gender;
+      gender = 'F';
+
+      String shortSummary = "My age is " + age + ", I was born in " + yearOfBirth +
+              " and my phone number is " + phoneNumber + '.';
+
+      System.out.println(shortSummary);
+      System.out.println(BodyTemperature);
+      System.out.println(height);
+      System.out.println(newMember);
+      System.out.println(gender);
         }
     }
